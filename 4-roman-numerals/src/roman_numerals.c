29f651438325e0f82415df950017fce19bb26e2c
Chesknut <csy003@fiu.edu>
2020-09-16 16:24:09 -0400

    Move structs outside of function

--- 4-roman-numerals/src/roman_numerals.c
+++ 4-roman-numerals/src/roman_numerals.c
@@ -13,32 +13,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct RomanNumeralKeyValues
+{
+   char *romanNumeralKey;
+   unsigned int normalNumberVal;
+};
+
+struct RomanNumeralKeyValues romanNumeralArr[13] = {
+   { "I", 1 },
+   { "IV", 4 },
+   { "V", 5 },
+   { "IX", 9 },
+   { "X", 10 },
+   { "XL", 40 },
+   { "L", 50 },
+   { "XC", 90 },
+   { "C", 100 },
+   { "CD", 400 },
+   { "D", 500 },
+   { "CM", 900 },
+   { "M", 1000 }
+};
+
 char *to_roman_numeral(unsigned int number)
 {
    // Set to 13 because max length of roman numeral conversion string is 13 (under 3000)
    char *romanNumeralConversion = calloc(13, 1);
-
-   struct RomanNumeralKeyValues
-   {
-      char *romanNumeralKey;
-      unsigned int normalNumberVal;
-   };
-
-   struct RomanNumeralKeyValues romanNumeralArr[13] = {
-      { "I", 1 },
-      { "IV", 4 },
-      { "V", 5 },
-      { "IX", 9 },
-      { "X", 10 },
-      { "XL", 40 },
-      { "L", 50 },
-      { "XC", 90 },
-      { "C", 100 },
-      { "CD", 400 },
-      { "D", 500 },
-      { "CM", 900 },
-      { "M", 1000 }
-   };
 
    for(unsigned int i = 13; i-- > 0;)
    {
